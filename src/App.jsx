9f052af308eddf8c968615f767edd2876cb0c969
Chris Tian <ctian1@users.noreply.github.com>
2021-11-05 00:12:16 -0700

    Add chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
+import {Box, ChakraProvider, FormControl, FormLabel, Switch} from '@chakra-ui/react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Box>
+            <FormControl display="flex" alignItems="center">
+              <FormLabel htmlFor="example-checkbox" mb="0">
+                Example
+              </FormLabel>
+              <Switch id="example-checkbox" />
+            </FormControl>
+          </Box>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
