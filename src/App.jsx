5ab4e8296edece9a6fc102125825bbe27c5f4eb7
Jane Vo <vo.janehanh@gmail.com>
2021-11-28 01:53:54 -0800

    Create starter routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
 import React from 'react';
-import { Box, ChakraProvider, FormControl, FormLabel, Switch } from '@chakra-ui/react';
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Box>
-            <FormControl display="flex" alignItems="center">
-              <FormLabel htmlFor="example-checkbox" mb="0">
-                Example
-              </FormLabel>
-              <Switch id="example-checkbox" />
-            </FormControl>
-          </Box>
-        </header>
-      </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" />
+          <Route exact path="/account" />
+          <Route exact path="/surveys" />
+          {/* admin specific routes (TO DO, specify admin) */}
+          <Route exact path="/sections" />
+          <Route exact path="/people" />
+        </Routes>
+      </Router>
     </ChakraProvider>
   );
 }
