cc0a64c1e9970c7d8d96241bcbb996802f788a7c
Dingyi (Jaedyn) Chang <dingyichang2020@gmail.com>
2022-01-14 16:57:13 -0800

    Added functionality to buttons and add section demo

--- src/components/Segment.jsx
+++ src/components/Segment.jsx
@@ -1,6 +1,48 @@
 import { React } from 'react';
-import { Button, Tr, Td, Text, Flex } from '@chakra-ui/react';
+import {
+  Button,
+  Tr,
+  Td,
+  Text,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { PropTypes } from 'prop-types';
+
+function UpdateSegmentPopup() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button backgroundColor="#A0AEC0" color="white" size="sm" onClick={onOpen}>
+        Update
+      </Button>
+
+      <Modal size="xl" isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Update Segment</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div>*UPDATE SEGMENT HERE*</div>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Save
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 const Segment = ({ segment, segmentName, distance }) => {
   return (
@@ -10,46 +52,12 @@ const Segment = ({ segment, segmentName, distance }) => {
       <Td>
         <Flex justifyContent="space-between">
           <Text>{distance}</Text>
-          <Button backgroundColor="#A0AEC0" color="white" size="sm">
-            Update
-          </Button>
+          {UpdateSegmentPopup()}
         </Flex>
       </Td>
     </Tr>
   );
 };
-
-// const Segment = ({ segment, segmentName, distance, showUpdate }) => {
-//   return (
-//     <Box align="center">
-//       <Flex direction="row">
-//         <Box flex="0.5">
-//           <Text>segment</Text>
-//           <Text>{segment}</Text>
-//         </Box>
-//         <Box flex="0.75" />
-//         <Box flex="1.5">
-//           <Text>name</Text>
-//           <Text>{segmentName}</Text>
-//         </Box>
-//         <Box flex="1">
-//           <Text>distance</Text>
-//           <Text>{distance}</Text>
-//         </Box>
-//         <Box flex="1">
-//           {showUpdate ? (
-//             <Button colorScheme="gray" size="sm">
-//               Update
-//             </Button>
-//           ) : (
-//             <></>
-//           )}
-//         </Box>
-//         <Box flex="1" />
-//       </Flex>
-//     </Box>
-//   );
-// };
 
 Segment.propTypes = {
   segment: PropTypes.string.isRequired,
