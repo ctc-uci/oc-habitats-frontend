e95c0100ca5515173b021236484b5826a8271621
Jane Vo <vo.janehanh@gmail.com>
2022-01-26 23:16:58 -0800

    Fix route elements

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import AccountPage from './pages/AccountPage';
 import SectionPage from './pages/SectionPage';
+import MonitorLogPage from './pages/MonitorLogPage';
+import EndangeredSpecies from './pages/EndangeredSpecies';
 
 function App() {
   return (
@@ -14,9 +16,13 @@ function App() {
           {/* Add routes as needed; route names subject to change */}
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/account" element={<AccountPage />} />
-          <Route exact path="/account" />
-          <Route exact path="/create-log" />
+          <Route exact path="/create-log" element={<MonitorLogPage />} />
           <Route exact path="/sections" element={<SectionPage />} />
+          <Route
+            exact
+            path="/endangered"
+            element={<EndangeredSpecies adultName="Snowy Plovers" />}
+          />
           {/* Admin only routes (TO DO, make admin only) */}
           <Route exact path="/people" />
           <Route exact path="/logs" />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { React } from 'react';
 const HomePage = props => {
   return (
     // eslint-disable-next-line react/no-unescaped-entities
-    <div>"side note can you just change the homepage text to 'Homepage' lol" - Jane Vo, 2022</div>
+    <div>Homepage</div>
   );
 };
 
