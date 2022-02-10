2dc6269f8077a7aa5a8ba81cc543f16438d0b1a8
Jacqueline Thai <jthai1201@gmail.com>
2022-02-09 18:10:48 -0800

    Added the People Page header and add new profile button.

--- src/pages/PeoplePage.jsx
+++ src/pages/PeoplePage.jsx
@@ -1,19 +1,30 @@
 import { React } from 'react';
-import { VStack, Container, Stack, Heading } from '@chakra-ui/react';
+import { VStack, Container, Heading, Button } from '@chakra-ui/react';
 import App from '../components/Table/Table';
 /*
 <Stack align="left" mb="3em" height="5px">
           <Heading as="h3" align="left">People</Heading>
         </Stack>
 */
+
 const PeoplePage = () => {
   return (
-    <VStack spacing="40px" align="left">
-      <header></header>
-      <App />
-      <header></header>
-      <App />
-    </VStack>
+    <>
+      <Container maxW="container.xl">
+        <Heading fontWeight="600" fontSize="36px" mb="40px" mt="40px" align="left">
+          People
+        </Heading>
+        <Button colorScheme="teal" variant="solid">
+          Create New Account
+        </Button>
+        <VStack spacing="40px" align="left">
+          <header></header>
+          <App />
+          <header></header>
+          <App />
+        </VStack>
+      </Container>
+    </>
   );
 };
 export default PeoplePage;
