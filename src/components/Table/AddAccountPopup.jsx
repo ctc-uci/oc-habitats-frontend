9645553f363a556febbd8fea02596162fb8c750d
Jacqueline Thai <jthai1201@gmail.com>
2022-02-15 16:48:44 -0800

    Added the input fields that change based on radio button selection. Also made Create New Account button to be disabled when all the fields are not filled for each selection

--- src/components/Table/AddAccountPopup.jsx
+++ src/components/Table/AddAccountPopup.jsx
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import { useState, React } from 'react';
 import {
   Button,
   useDisclosure,
@@ -12,10 +12,18 @@ import {
   RadioGroup,
   Radio,
   Stack,
+  Input,
 } from '@chakra-ui/react';
 
 function AddAccountPopup() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [value, setValue] = useState('0');
+  const [volInput, setVolInput] = useState('');
+  const [adminName, setAdminName] = useState('');
+  const [adminEmail, setAdminEmail] = useState('');
+  const volHandleChange = event => setVolInput(event.target.value);
+  const adminNameHandleChange = event => setAdminName(event.target.value);
+  const adminEmailHandleChange = event => setAdminEmail(event.target.value);
   return (
     <>
       <Button onClick={onOpen} colorScheme="blue" variant="solid">
@@ -28,20 +36,40 @@ function AddAccountPopup() {
           <ModalHeader>Create New User</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <RadioGroup>
+            <RadioGroup onChange={setValue} value={value}>
               <Stack column="vertical">
-                <Radio value="inside">Volunteer</Radio>
-                <Radio value="outside">Admin</Radio>
+                <Radio value="1">Volunteer</Radio>
+                <Radio value="2">Admin</Radio>
               </Stack>
             </RadioGroup>
-            {/* <Lorem count={2} /> */}
+            {value === '1' && (
+              <Stack columnt="vertical">
+                <p>Enter volunteer&apos;s email:</p>
+                <Input variant="filled" value={volInput} onChange={volHandleChange} />
+              </Stack>
+            )}
+            {value === '2' && (
+              <Stack column="vertical">
+                <p>Enter admin&apos;s name:</p>
+                <Input variant="filled" value={adminName} onChange={adminNameHandleChange} />
+                <p>
+                  Enter admin&apos;s OCH email:
+                  <Input variant="filled" value={adminEmail} onChange={adminEmailHandleChange} />
+                </p>
+              </Stack>
+            )}
           </ModalBody>
 
           <ModalFooter>
             <Button colorScheme="gray" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button colorScheme="blue">Create New User</Button>
+            <Button
+              colorScheme="blue"
+              disabled={volInput === '' && (adminEmail === '' || adminName === '')}
+            >
+              Create New User
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
