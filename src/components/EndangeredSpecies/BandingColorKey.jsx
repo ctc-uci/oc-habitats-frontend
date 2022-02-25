af6be3e46560220353c377e8d75c2b6b58d4f7de
Chris Tian <ctian1@users.noreply.github.com>
2022-02-25 13:50:57 -0800

    Update band colors, move to file

--- src/components/EndangeredSpecies/BandingColorKey.jsx
+++ src/components/EndangeredSpecies/BandingColorKey.jsx
@@ -1,5 +1,6 @@
 import { React } from 'react';
 import { Box, HStack, VStack, Text } from '@chakra-ui/react';
+import BandColors from '../../common/BandColors';
 
 const BandingColorKey = () => {
   const createRow = (colorName, colorCode) => {
@@ -17,19 +18,7 @@ const BandingColorKey = () => {
       <Box textAlign="end" w="max-content" position="absolute" marginTop="60px" right="60px">
         <Text as="b">Banding Color Key</Text>
         <VStack spacing={3} alignItems="end" pt={4}>
-          {createRow('Aqua (A)', 'teal.200')}
-          {createRow('Blue (B)', 'blue.400')}
-          {createRow('Green (G)', 'green.500')}
-          {createRow('Black (K)', 'black')}
-          {createRow('Lime (L)', '#B7D368')}
-          {createRow('Brown/Tan (N)', 'yellow.800')}
-          {createRow('Orange (O)', 'orange.400')}
-          {createRow('Pink (P)', 'pink.400')}
-          {createRow('Red (R)', 'red.600')}
-          {createRow('Silver (S)', 'gray.200')}
-          {createRow('Violet (V)', 'purple.400')}
-          {createRow('White (W)', 'white')}
-          {createRow('Yellow (Y)', 'yellow.200')}
+          {BandColors.map(color => createRow(color.label, color.realColor))}
         </VStack>
       </Box>
     </Box>
