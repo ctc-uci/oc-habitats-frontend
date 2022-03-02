29ac2b573ec3fd55b9ea23a6242a60745d0db713
Jane Vo <vo.janehanh@gmail.com>
2022-03-01 19:38:30 -0800

    Cleanup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { HStack, Text, Flex, Button, Avatar, Image } from '@chakra-ui/react';
 import logo from '../../assets/OCHlogo.png';
 
-const Navbar = isAdmin => {
+const Navbar = ({ isAdmin }) => {
   const navbarLink = (text, path = '/') => {
     return (
       <Link to={path}>
@@ -13,52 +13,55 @@ const Navbar = isAdmin => {
     );
   };
 
-  // TO DO: get profile image
+  // TO DO: get profile image and name
 
-  const adminNav = () => {
+  const adminLinks = () => {
     return (
-      <Flex as="nav" bgColor="#4E4E4E" color="white" align="center" justify="space-between">
-        <Link to="/">
-          <Image pl={4} maxW="30%" src={logo} alt="logo" />
-        </Link>
-        <HStack spacing={12} justify="flex-end" pr={4}>
-          {navbarLink('Monitor Logs', '/logs')}
-          {navbarLink('People', '/people')}
-          {navbarLink('Species List', '/species')}
-          {navbarLink('Map', '/map')}
-          {navbarLink('Contacts')}
-          <Link to="/account">
-            <Avatar m={1} name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
-          </Link>
-        </HStack>
-      </Flex>
+      <>
+        {navbarLink('Monitor Logs', '/logs')}
+        {navbarLink('People', '/people')}
+        {navbarLink('Species List', '/species')}
+        {navbarLink('Map', '/map')}
+        {navbarLink('Contacts')}
+      </>
     );
   };
 
-  const volunteerNav = () => {
+  const volunteerLinks = () => {
     return (
-      <Flex as="nav" bgColor="#4E4E4E" color="white" align="center" justify="space-between">
-        <Link to="/">
-          <Image pl={4} maxW="30%" src={logo} alt="logo" />
+      <>
+        {navbarLink('Monitor Logs', '/logs')}
+        {navbarLink('Species List', '/species')}
+        {navbarLink('Map', '/map')}
+        <Link to="/create-log">
+          <Button size="sm" bgColor="#2BC0E3" color="black">
+            Start Session
+          </Button>
         </Link>
-        <HStack spacing={12} justify="flex-end" pr={4}>
-          {navbarLink('Monitor Logs', '/logs')}
-          {navbarLink('Species List', '/species')}
-          {navbarLink('Map', '/map')}
-          <Link to="/create-log">
-            <Button size="sm" bgColor="#2BC0E3" color="black">
-              Start Session
-            </Button>
-          </Link>
-          <Link to="/account">
-            <Avatar m={1} name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
-          </Link>
-        </HStack>
-      </Flex>
+      </>
     );
   };
 
-  return isAdmin ? adminNav() : volunteerNav();
+  return (
+    <Flex
+      as="nav"
+      bgColor="#4E4E4E"
+      color="white"
+      align="center"
+      justify="space-between"
+      style={{ position: 'sticky', top: 0 }}
+    >
+      <Link to="/">
+        <Image pl={4} maxW="30%" src={logo} alt="logo" />
+      </Link>
+      <HStack spacing={12} justify="flex-end" pr={4}>
+        {isAdmin ? adminLinks() : volunteerLinks()}
+        <Link to="/account">
+          <Avatar m={1} name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+        </Link>
+      </HStack>
+    </Flex>
+  );
 };
 
 Navbar.propTypes = {
