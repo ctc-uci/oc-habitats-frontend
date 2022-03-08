d358e28af8e5f12c4078accb469e0d2e9602f9ca
Jane Vo <vo.janehanh@gmail.com>
2022-03-08 03:11:46 -0800

    Add colors

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -5,6 +5,12 @@ const theme = extendTheme({
     heading: 'Inter',
     body: 'Inter',
   },
+  colors: {
+    ochBlue: '#2BC0E3',
+    ochGrey: '#4E4E4E',
+    ochOrange: '#F49923',
+    ochBlack: '#231F20',
+  },
 });
 
 export default theme;
