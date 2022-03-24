fd8cf24ec4eb78bd42c2c6cc63f41d0e1493bc5e
Chris Tian <ctian1@users.noreply.github.com>
2022-03-24 16:52:51 -0700

    Update banding form

--- src/components/EndangeredSpecies/BandingSection.jsx
+++ src/components/EndangeredSpecies/BandingSection.jsx
@@ -9,7 +9,6 @@ import {
   TabPanels,
   TabPanel,
   Box,
-  FormControl,
   FormLabel,
   Code,
   Table,
@@ -18,15 +17,24 @@ import {
   Thead,
   Input,
   Select as ChakraSelect,
+  Grid,
+  GridItem,
+  Text,
+  Checkbox,
+  Tbody,
+  chakra,
 } from '@chakra-ui/react';
 import { Select, chakraComponents } from 'chakra-react-select';
-import update from 'immutability-helper';
-import { CloseIcon } from '@chakra-ui/icons';
+import { Controller, useFieldArray, useFormContext } from 'react-hook-form';
+import { BsFillCircleFill } from 'react-icons/bs';
+import { IconContext } from 'react-icons';
 import BandColors from '../../common/BandColors';
 import CloseableTab from './CloseableTab';
 
+const BAND_POSITIONS = ['Top Left', 'Top Right', 'Bottom Left', 'Bottom Right'];
+
 const generateSingleBandCode = band => {
-  if (band.leg === null || band.verticalPosition === null || band.colors.length === 0) return '';
+  if (!band.verticalPosition || !band.colors || band.colors.length === 0) return '';
   let color;
   if (band.colors.length === 1) {
     color = band.colors[0].realValue;
@@ -37,31 +45,23 @@ const generateSingleBandCode = band => {
     color = color.toLowerCase();
   }
   if (band.alphanumeric && band.alphanumeric !== '') {
-    return `${color}.${band.alphanumeric}`;
+    return `${band.flag ? 'F' : ''}${color}.${band.alphanumeric}`;
   }
-  return color;
+  return `${band.flag ? 'F' : ''}${color}`;
 };
 const generateCode = bands => {
-  let topLeft = '';
-  let topRight = '';
-  let bottomLeft = '';
-  let bottomRight = '';
-  bands.forEach(band => {
-    const code = generateSingleBandCode(band);
-    if (band.leg === 'left') {
-      if (band.verticalPosition === 'above') {
-        topLeft += code;
-      } else {
-        bottomLeft += code;
-      }
-    } else if (band.verticalPosition === 'above') {
-      topRight += code;
-    } else {
-      bottomRight += code;
-    }
-  });
-  const leftCode = topLeft + bottomLeft;
-  const rightCode = topRight + bottomRight;
+  if (!bands || !bands[0]) {
+    return 'invalid';
+  }
+  const [topLeft, topRight, bottomLeft, bottomRight] = bands;
+  if (
+    (topLeft.verticalPosition === 'below' && bottomLeft.verticalPosition === 'above') ||
+    (topRight.verticalPosition === 'below' && bottomRight.verticalPosition === 'above')
+  ) {
+    return 'Top band must be above bottom band';
+  }
+  const leftCode = generateSingleBandCode(topLeft) + generateSingleBandCode(bottomLeft);
+  const rightCode = generateSingleBandCode(topRight) + generateSingleBandCode(bottomRight);
   // if a leg has no bands, put X instead
   return `${leftCode || 'X'}:${rightCode || 'X'}`;
 };
@@ -69,68 +69,65 @@ const generateCode = bands => {
 const BandingSection = () => {
   const title = 'Banding';
 
+  const { register, watch, control } = useFormContext();
+
   // tabs can be added for each bird that they want to report bands for
-  const [birdBandTabs, setBirdBandTabs] = useState([]);
+  const {
+    fields: birdBandTabs,
+    append,
+    remove,
+  } = useFieldArray({
+    control,
+    name: 'bandTabs',
+  });
   // keep track of current active tab
   const [activeTabIndex, setActiveTabIndex] = useState(0);
+  // watch the 4 bands on the current tab so we can generate the band code
+  const currentBandTab = [0, 1, 2, 3].map(idx => watch(`bandTabs.${activeTabIndex}.${idx}`));
+  const currentBandTabCode = generateCode(currentBandTab);
 
   const addBirdBandTab = () => {
-    setBirdBandTabs([
-      ...birdBandTabs,
-      [{ colors: [], leg: null, verticalPosition: null, alphanumeric: '' }],
-    ]);
+    const newTab = {};
+    [...Array(4)].forEach((_, idx) => {
+      newTab[idx] = {
+        colors: [],
+        flag: false,
+        verticalPosition: null,
+        alphanumeric: '',
+      };
+    });
+    append(newTab);
   };
 
   const removeBirdBandTab = idx => {
-    setBirdBandTabs(update(birdBandTabs, { $splice: [[idx, 1]] }));
+    remove(idx);
   };
 
-  // add a row to the current bird's tab that represents a single band
-  const addBandRow = () => {
-    setBirdBandTabs(
-      update(birdBandTabs, {
-        [activeTabIndex]: {
-          $push: [{ colors: [], leg: null, verticalPosition: null, alphanumeric: '' }],
-        },
-      }),
-    );
-  };
-
-  const updateBandRow = (rowIdx, key, val) => {
-    setBirdBandTabs(
-      update(birdBandTabs, {
-        [activeTabIndex]: {
-          [rowIdx]: {
-            [key]: { $set: val },
-          },
-        },
-      }),
-    );
-  };
-
-  // remove a band row from current bird's tab
-  const removeBandRow = rowIdx => {
-    setBirdBandTabs(
-      update(birdBandTabs, {
-        [activeTabIndex]: {
-          $splice: [[rowIdx, 1]],
-        },
-      }),
-    );
-  };
-
+  // setup custom rendering to show the band color in color select dropdown
   const customComponents = {
     // eslint-disable-next-line react/prop-types
     Option: ({ children, ...props }) => (
       // eslint-disable-next-line react/jsx-props-no-spreading
       <chakraComponents.Option {...props}>
         {/* eslint-disable-next-line react/prop-types */}
-        {props.data.selectIcon} {children}
+        <chakra.span
+          marginRight={2}
+          borderWidth="1px"
+          // eslint-disable-next-line react/prop-types
+          borderColor={props.data.value === 'W' ? 'gray.600' : props.data.realColor}
+          borderRadius="3xl"
+        >
+          {/* eslint-disable-next-line react/prop-types */}
+          <IconContext.Provider value={{ color: props.data.realColor }}>
+            <BsFillCircleFill />
+          </IconContext.Provider>
+        </chakra.span>{' '}
+        {children}
       </chakraComponents.Option>
     ),
   };
 
-  const mappedColourOptions = BandColors.map(option => ({
+  const mappedColorOptions = BandColors.map(option => ({
     ...option,
     colorScheme: option.selectColor,
     realValue: option.value,
@@ -138,16 +135,12 @@ const BandingSection = () => {
     value: Math.random(),
   }));
 
-  // const removeBandTab = useCallback(idx => {
-  //   setBandTabs(update(bandTabs, { $remove: [idx] }));
-  // });
-
   const addTabs = () => {
     return birdBandTabs.map((d, i) => {
       return (
         <CloseableTab
           // eslint-disable-next-line react/no-array-index-key
-          key={i}
+          key={d.id}
           onClose={() => removeBirdBandTab(i)}
         >
           Banded Bird {i + 1}
@@ -157,12 +150,24 @@ const BandingSection = () => {
   };
 
   return (
-    <Box width="container.lg">
+    <Box width="100%">
       <VStack spacing="4" justify="start" align="start">
         <Heading as="h3" size="md">
           {title}
         </Heading>
-        <Box>Note: Input the bands in top to bottom order (from the bird’s perspective).</Box>
+        <HStack align="start">
+          <Text fontWeight="semibold">Note:</Text>
+          <Box>
+            <Text>
+              If there is an alphanumeric band, select the color(s) of the band and input the
+              alphanumeric code in the text field “Alphanumeric”.
+            </Text>
+            <Text>
+              If there is an alphanumeric flag on the bird’s leg, check the “Is an Alphanumeric
+              Flag” box.
+            </Text>
+          </Box>
+        </HStack>
         <HStack marginTop="18px" />
         <Tabs
           variant="solid-rounded"
@@ -187,6 +192,7 @@ const BandingSection = () => {
                 variant="outline"
                 borderRadius="full"
                 colorScheme="cyan"
+                minWidth="180px"
               >
                 Add Banded Bird +
               </Button>
@@ -194,112 +200,92 @@ const BandingSection = () => {
           </TabList>
           <TabPanels>
             {birdBandTabs.map((birdBandTab, tabIndex) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <TabPanel bgColor="gray.50" key={tabIndex} padding={4} marginTop={2} rounded="md">
-                <Table variant="unstyled" size="sm">
-                  <Thead>
-                    <Td width="5%" />
-                    <Td width="35%">
-                      <FormLabel>Band Color(s)</FormLabel>
-                    </Td>
-                    <Td width="20%">
-                      <FormLabel>Band Leg</FormLabel>
-                    </Td>
-                    <Td width="20%">
-                      <FormLabel>Band Ankle Position</FormLabel>
-                    </Td>
-                    <Td width="20%">
-                      <FormLabel>Alphanumeric Code (Optional)</FormLabel>
-                    </Td>
-                  </Thead>
-                  {birdBandTab.map((row, rowIdx) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <Tr key={rowIdx}>
-                      <Td>
-                        <Button
-                          size="sm"
-                          colorScheme="gray"
-                          backgroundColor="gray.200"
-                          onClick={() => removeBandRow(rowIdx)}
-                        >
-                          <CloseIcon />
-                        </Button>
-                      </Td>
-                      <Td>
-                        <FormControl backgroundColor="white">
-                          <Select
-                            isMulti
-                            name="colors"
-                            options={mappedColourOptions}
-                            closeMenuOnSelect={false}
-                            components={customComponents}
-                            onChange={selectedBands =>
-                              updateBandRow(rowIdx, 'colors', selectedBands)
-                            }
-                            value={row.colors}
-                          />
-                        </FormControl>
-                      </Td>
-                      <Td>
-                        <FormControl backgroundColor="white">
-                          <ChakraSelect
-                            onChange={e => updateBandRow(rowIdx, 'leg', e.target.value)}
-                            placeholder="Select..."
-                            value={row.leg}
-                          >
-                            <option value="left">Left</option>
-                            <option value="right">Right</option>
-                          </ChakraSelect>
-                        </FormControl>
-                      </Td>
-                      <Td>
-                        <FormControl backgroundColor="white">
-                          <ChakraSelect
-                            onChange={e =>
-                              updateBandRow(rowIdx, 'verticalPosition', e.target.value)
-                            }
-                            placeholder="Select..."
-                            value={row.verticalPosition}
-                          >
-                            <option value="above">Above ankle</option>
-                            <option value="below">Below ankle</option>
-                          </ChakraSelect>
-                        </FormControl>
-                      </Td>
-                      <Td>
-                        <FormControl backgroundColor="white">
-                          <Input
-                            onChange={e => updateBandRow(rowIdx, 'alphanumeric', e.target.value)}
-                            value={row.alphanumeric}
-                          />
-                        </FormControl>
-                      </Td>
-                    </Tr>
+              <TabPanel
+                bgColor="gray.50"
+                key={birdBandTab.id}
+                padding={4}
+                marginTop={2}
+                rounded="md"
+              >
+                <Grid templateColumns={['repeat(1,1fr)', '', 'repeat(2, 1fr)']} gap="2em">
+                  {BAND_POSITIONS.map((bandPosition, idx) => (
+                    <GridItem key={bandPosition}>
+                      <Text fontWeight="semibold" fontSize="md" mb="2">
+                        {bandPosition} Band
+                      </Text>
+                      <Table variant="unstyled" size="sm">
+                        <Thead>
+                          <Tr>
+                            <Td width="25%" p="0" pr="4">
+                              <Text fontSize="sm" mb="1">
+                                Position on leg
+                              </Text>
+                            </Td>
+                            <Td width="45%" p="0" pr="4">
+                              <Text fontSize="sm" mb="1">
+                                Color(s)
+                              </Text>
+                            </Td>
+                            <Td width="30%" p="0">
+                              <Text fontSize="sm" mb="1">
+                                Alphanumeric code
+                              </Text>
+                            </Td>
+                          </Tr>
+                        </Thead>
+                        <Tbody>
+                          <Tr>
+                            <Td p="0" pr="4" verticalAlign="top">
+                              <ChakraSelect
+                                placeholder="Select..."
+                                bgColor="white"
+                                {...register(`bandTabs.${tabIndex}.${idx}.verticalPosition`)}
+                              >
+                                <option value="above">Above ankle</option>
+                                <option value="below">Below ankle</option>
+                              </ChakraSelect>
+                            </Td>
+                            <Td p="0" pr="4" verticalAlign="top">
+                              <Box bgColor="white">
+                                <Controller
+                                  name={`bandTabs.${tabIndex}.${idx}.colors`}
+                                  control={control}
+                                  render={({ field }) => (
+                                    <Select
+                                      {...field}
+                                      isMulti
+                                      options={mappedColorOptions}
+                                      closeMenuOnSelect={false}
+                                      components={customComponents}
+                                    />
+                                  )}
+                                />
+                              </Box>
+                            </Td>
+                            <Td p="0" verticalAlign="top">
+                              <Input
+                                bgColor="white"
+                                {...register(`bandTabs.${tabIndex}.${idx}.alphanumeric`)}
+                              />
+                            </Td>
+                          </Tr>
+                          <Tr>
+                            <Td px="0" py="2" colSpan={3}>
+                              <Checkbox {...register(`bandTabs.${tabIndex}.${idx}.flag`)}>
+                                Is an Alphanumeric Flag
+                              </Checkbox>
+                            </Td>
+                          </Tr>
+                        </Tbody>
+                      </Table>
+                    </GridItem>
                   ))}
-                  <Tr>
-                    <Td />
-                    <Td>
-                      <Button
-                        width="100%"
-                        marginTop={2}
-                        marginBottom={2}
-                        onClick={addBandRow}
-                        colorScheme="cyan"
-                      >
-                        Add Band +
-                      </Button>
-                    </Td>
-                  </Tr>
-                  <Tr>
-                    <Td />
-                    <Td>
-                      <FormLabel>Generated Banding Code</FormLabel>
+                  <GridItem>
+                    <FormLabel>Generated Banding Code</FormLabel>
 
-                      {/* <Input maxWidth="2xs" disabled value={generateCode(bandTabs[tabIndex])} /> */}
-                      <Code>{generateCode(birdBandTabs[tabIndex])}</Code>
-                    </Td>
-                  </Tr>
-                </Table>
+                    <Code fontSize="lg">{currentBandTabCode}</Code>
+                  </GridItem>
+                </Grid>
               </TabPanel>
             ))}
           </TabPanels>
