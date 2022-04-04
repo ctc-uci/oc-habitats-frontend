dcac52e95caf5c8fc6c8255ac19eec8941fcbd89
Chris Tian <ctian1@users.noreply.github.com>
2022-04-03 18:43:00 -0700

    Add breadcrumbs, confirm close popup, floating footer

--- src/pages/EndangeredSpeciesPopup.jsx
+++ src/pages/EndangeredSpeciesPopup.jsx
@@ -1,6 +1,6 @@
-import { React, useState } from 'react';
+import { React, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { ArrowBackIcon } from '@chakra-ui/icons';
+import { ArrowBackIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import {
   Container,
   Heading,
@@ -10,9 +10,23 @@ import {
   Button,
   HStack,
   IconButton,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  Box,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Flex,
+  Spacer,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
 } from '@chakra-ui/react';
 
 import { useForm, FormProvider } from 'react-hook-form';
+import { FiArrowUp } from 'react-icons/fi';
 import GeneralListedInformation from '../components/EndangeredSpecies/GeneralListedInformation';
 import Location from '../components/EndangeredSpecies/Location';
 import options from '../components/EndangeredSpecies/DropdownOptions';
@@ -36,31 +50,69 @@ const EndangeredSpeciesPopup = ({ closeModal, adultName, addRow }) => {
     },
   });
 
-  const time2 = formMethods.watch('time');
+  const confirmCloseModal = useDisclosure();
+  const topRef = useRef();
 
   const handleSubmit = event => {
     event.preventDefault();
     const formData = formMethods.getValues();
 
-    // eslint-disable-next-line no-console
     console.log('formData', formData);
     addRow(formData);
     closeModal();
   };
 
+  const returnToTop = () => {
+    topRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <FormProvider {...formMethods}>
-      <HStack w="100%">
-        <IconButton
-          icon={<ArrowBackIcon boxSize={10} />}
-          bgColor="transparent"
-          position="fixed"
-          top="16px"
-          left="16px"
-          onClick={closeModal}
-        />
+      <HStack w="100%" ref={topRef}>
+        <Flex mt="16px" w="100%">
+          <IconButton
+            icon={<ArrowBackIcon boxSize={10} />}
+            bgColor="transparent"
+            onClick={confirmCloseModal.onOpen}
+            left="16px"
+          />
+          <Modal isOpen={confirmCloseModal.isOpen} onClose={confirmCloseModal.onClose}>
+            <ModalOverlay />
+            <ModalContent rounded="none">
+              <ModalHeader>Careful - there&apos;s unsaved changes</ModalHeader>
+              <ModalBody>
+                If you leave this page, the data here will NOT be saved. Are you sure you’d like to
+                leave this page?
+              </ModalBody>
+
+              <ModalFooter>
+                <Button mr="3" onClick={confirmCloseModal.onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" onClick={closeModal}>
+                  Discard changes
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+          <Spacer />
+          <Breadcrumb
+            position="relative"
+            right="16px"
+            spacing="8px"
+            separator={<ChevronRightIcon color="gray.500" />}
+          >
+            <BreadcrumbItem>
+              <BreadcrumbLink onClick={confirmCloseModal.onOpen}>Survey Log</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbItem isCurrentPage>
+              <BreadcrumbLink>Add {adultName}</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Flex>
       </HStack>
-      <Container maxW="container.xl" paddingTop="40px" paddingBottom="40px">
+      <Container maxW="container.xl" paddingTop="40px" paddingBottom="100px">
         <Stack align="center" mb="3em">
           <Heading as="h1">Add {adultName}</Heading>
         </Stack>
@@ -82,11 +134,33 @@ const EndangeredSpeciesPopup = ({ closeModal, adultName, addRow }) => {
               />
             </VStack>
           </VStack>
-          <Button mt={4} colorScheme="teal" type="submit">
-            Submit
-          </Button>
         </form>
       </Container>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        bg="ochGrey"
+        position="fixed"
+        w="100%"
+        bottom="0"
+        h="16"
+        zIndex="banner"
+      >
+        <Flex width="100%" maxWidth="container.xl" padding="4">
+          <Button
+            onClick={returnToTop}
+            variant="outline"
+            color="white"
+            _hover={{ color: 'black', backgroundColor: 'white' }}
+          >
+            Return to Top <FiArrowUp style={{ marginLeft: '4px' }} />
+          </Button>
+          <Spacer />
+          <Button colorScheme="cyan" type="submit">
+            Add to Tracker
+          </Button>
+        </Flex>
+      </Flex>
     </FormProvider>
   );
 };
