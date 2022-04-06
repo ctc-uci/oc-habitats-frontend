babd38bfc18cc7345d653f77604839e6b2c14f86
Rochelle Nixon <rochelln@uci.edu>
2022-04-05 23:15:45 -0700

    Added profile dropdown

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { useState, React } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { HStack, Flex, Button, Avatar, Image } from '@chakra-ui/react';
+import {
+  HStack,
+  Flex,
+  Button,
+  Avatar,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+import { FaSignOutAlt } from 'react-icons/fa';
 import NavbarLink from './NavbarLink';
 import logo from '../../assets/OCHlogo.png';
 
 const Navbar = ({ isAdmin }) => {
+  const [adminPortal, setAdminPortal] = useState(true);
   // TO DO: get profile image and name
   const user = {
     firstName: 'Dan',
@@ -27,6 +39,14 @@ const Navbar = ({ isAdmin }) => {
     { text: 'Sections & Segments', path: '/sections' },
   ];
 
+  const togglePortal = () => {
+    if (adminPortal) {
+      setAdminPortal(false);
+    } else {
+      setAdminPortal(true);
+    }
+  };
+
   return (
     <Flex
       as="nav"
@@ -44,19 +64,58 @@ const Navbar = ({ isAdmin }) => {
       </Link>
       {/* TO DO: if user is not signed in, only logo */}
       <HStack h="inherit" spacing={6} pr={4}>
-        {isAdmin
+        {isAdmin && adminPortal
           ? admin.map(a => <NavbarLink key={a.text} text={a.text} path={a.path} />)
           : volunteer.map(v => <NavbarLink key={v.text} text={v.text} path={v.path} />)}
-        {!isAdmin && (
+        {(!isAdmin || (isAdmin && !adminPortal)) && (
           <Link to="/create-log">
             <Button size="sm" bgColor="ochBlue" color="ochBlack" _hover={{ opacity: '0.8' }}>
               Start Session
             </Button>
           </Link>
         )}
-        <Link to="/account">
-          <Avatar m={1} name={user.firstName + user.lastName} src={user.profilePic} />
-        </Link>
+        {!isAdmin && (
+          <Menu placement="right-start">
+            <MenuButton>
+              <Avatar m={1} name={user.firstName + user.lastName} src={user.profilePic} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={<Link to="/account" />} color="black" fontFamily="Inter">
+                Account
+              </MenuItem>
+              <MenuItem color="red" fontFamily="Inter">
+                {'Sign Out  '}
+                <FaSignOutAlt marginLeft="10" />
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        )}
+        {isAdmin && (
+          <Menu placement="right-start">
+            <MenuButton>
+              <Avatar m={1} name={user.firstName + user.lastName} src={user.profilePic} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={<Link to="/account" />} color="black" fontFamily="Inter">
+                Account
+              </MenuItem>
+              {adminPortal && (
+                <MenuItem color="black" fontFamily="Inter" onClick={togglePortal}>
+                  Volunteer Portal
+                </MenuItem>
+              )}
+              {!adminPortal && (
+                <MenuItem color="black" fontFamily="Inter" onClick={togglePortal}>
+                  Admin Portal
+                </MenuItem>
+              )}
+              <MenuItem color="red" fontFamily="Inter">
+                {'Sign Out  '}
+                <FaSignOutAlt marginLeft="10" />
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        )}
       </HStack>
     </Flex>
   );
