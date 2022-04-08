f9eb175d99e1ce618a4753d0712f92c0d9f0e5d7
Hayden-MP <hmpowers86@gmail.com>
2022-04-08 16:16:31 -0700

    Mobile version complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Flex, Text, StackDivider, HStack, Link, Image } from '@chakra-ui/react';
+import { Flex, Link, Image, Box, Stack } from '@chakra-ui/react';
 import facebook from '../../assets/facebook.png';
 import instagram from '../../assets/instagram.svg';
 import twitter from '../../assets/twitter.png';
+import './Footer.css';
 
 const Footer = () => {
   return (
     <Flex
+      id="footer-flex"
       as="footer"
       position="absolute"
       bottom={0}
@@ -17,26 +19,36 @@ const Footer = () => {
       color="white"
       bgColor="ochGrey"
     >
-      <HStack spacing={6} divider={<StackDivider borderColor="white" borderStyle="dashed" />}>
-        <Text>949.697.8651</Text>
-        <Flex direction="column" align="center">
-          <Text>och@ochabitats.org</Text>
+      <Stack direction="horizontal" id="footer-content">
+        <Box className="footer-box" p="2">
+          949.697.8651
+        </Box>
+        <Box className="footer-box" p="2">
+          och@ochabitats.org
+        </Box>
+        <Box className="footer-box" p="2">
+          <Link href="http://www.ochabitats.org" isExternal>
+            ochabitats.org
+          </Link>
+        </Box>
+        <Flex>
+          <Box className="icon" p="2">
+            <Link href="https://www.facebook.com/ochabitats" isExternal>
+              <Image maxH="50px" src={facebook} _hover={{ opacity: '0.8' }} />
+            </Link>
+          </Box>
+          <Box className="icon" p="2">
+            <Link href="https://twitter.com/ochabitats" isExternal>
+              <Image maxH="50px" src={twitter} _hover={{ opacity: '0.8' }} />
+            </Link>
+          </Box>
+          <Box className="icon" p="2">
+            <Link href="https://www.instagram.com/ochabitats/" isExternal>
+              <Image maxH="50px" src={instagram} _hover={{ opacity: '0.8' }} />
+            </Link>
+          </Box>
         </Flex>
-        <Link href="http://www.ochabitats.org" isExternal>
-          ochabitats.org
-        </Link>
-        <HStack spacing={3}>
-          <Link href="https://www.facebook.com/ochabitats" isExternal>
-            <Image maxH="52px" src={facebook} _hover={{ opacity: '0.8' }} />
-          </Link>
-          <Link href="https://twitter.com/ochabitats" isExternal>
-            <Image maxH="52px" src={twitter} _hover={{ opacity: '0.8' }} />
-          </Link>
-          <Link href="https://www.instagram.com/ochabitats/" isExternal>
-            <Image maxH="52px" src={instagram} _hover={{ opacity: '0.8' }} />
-          </Link>
-        </HStack>
-      </HStack>
+      </Stack>
     </Flex>
   );
 };
