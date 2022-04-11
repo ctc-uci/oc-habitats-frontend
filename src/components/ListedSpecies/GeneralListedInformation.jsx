d800fb43a8064533acf1cfa01050d78ba2527df5
Chris Tian <ctian1@users.noreply.github.com>
2022-04-10 17:42:30 -0700

    Update time input

--- src/components/ListedSpecies/GeneralListedInformation.jsx
+++ src/components/ListedSpecies/GeneralListedInformation.jsx
@@ -1,12 +1,10 @@
 import { InfoIcon } from '@chakra-ui/icons';
 import {
-  Button,
   Flex,
   FormControl,
   FormLabel,
   Input,
   InputGroup,
-  InputRightElement,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
@@ -16,29 +14,15 @@ import {
   Stack,
   Tooltip,
 } from '@chakra-ui/react';
-import { React, useEffect, useState } from 'react';
+import { React } from 'react';
 import { useFormContext } from 'react-hook-form';
 import CollapsibleSection from '../CollapsibleSection/CollapsibleSection';
 import options from './DropdownOptions';
 import footNotes from './FootNotes';
-import './GeneralListedInformation.css';
 
 // component/section name not final
 const GeneralListedInformation = () => {
   const { register, setValue, getValues } = useFormContext();
-  const [meridiem, setMeridiem] = useState(getValues().meridiem || 'AM');
-
-  useEffect(() => {
-    setValue('meridiem', meridiem);
-  }, [meridiem]);
-
-  const toggleTime = () => {
-    if (meridiem === 'AM') {
-      setMeridiem('PM');
-    } else {
-      setMeridiem('AM');
-    }
-  };
 
   const createOptions = () => {
     return options.habitat.map(option => {
@@ -117,24 +101,7 @@ const GeneralListedInformation = () => {
           <FormLabel>
             Time
             <InputGroup>
-              <Input
-                className="without-meridiem"
-                defaultValue="07:00"
-                type="time"
-                {...register('time')}
-              />
-              <InputRightElement w="4.5rem">
-                <Button
-                  h="2rem"
-                  w="3.9rem"
-                  size="sm"
-                  id="meridiem"
-                  value={meridiem}
-                  onClick={toggleTime}
-                >
-                  {meridiem}
-                </Button>
-              </InputRightElement>
+              <Input type="time" {...register('time')} />
             </InputGroup>
           </FormLabel>
         </FormControl>
