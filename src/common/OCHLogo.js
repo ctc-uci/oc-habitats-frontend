f0441b6b90e443c7ecdedb5427686eeab89285f0
Chris Tian <ctian1@users.noreply.github.com>
2022-04-19 01:34:35 -0700

    Fix email image

---
+++ src/common/OCHLogo.js
@@ -0,0 +1,5 @@
+// TODO: host ourselves
+const img =
+  'https://static.wixstatic.com/media/6ac8e8_573c5f20be55436e8cd8c5e104a4c550~mv2_d_1650_1275_s_2.png/v1/crop/x_341,y_449,w_994,h_360/fill/w_313,h_104,al_c,usm_0.66_1.00_0.01,enc_auto/6ac8e8_573c5f20be55436e8cd8c5e104a4c550~mv2_d_1650_1275_s_2.png';
+
+export default img;
--- src/components/Email/EmailTemplates/AdminInviteEmail.jsx
+++ src/components/Email/EmailTemplates/AdminInviteEmail.jsx
@@ -2,42 +2,36 @@
 /* eslint-disable react/jsx-no-target-blank */
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Email, Item } from 'react-html-email';
+import { Email, Image, Item } from 'react-html-email';
+import OCHLogo from '../../../common/OCHLogo';
 
 const AdminInviteEmail = ({ role, url }) => {
   return (
-    <div>
-      <Email title="">
-        <img
-          src={require('../../../assets/OCHlogo.png').default}
-          w="346px"
-          h="105px"
-          alt="OC Habitats Logo"
-        />
-        <Item>
-          <h1>{`Hello OC Habitats ${role === 'volunteer' ? 'Monitor' : 'Administrator'}!`}</h1>
-        </Item>
-        <Item>
-          <p>
-            An OC Habitats administrator has requested to grant you access to the OC Habitats
-            Digital Monitor Log system. To complete your registration, you will need to enter your
-            first and last name, and create a password.
-          </p>
-        </Item>
-        <Item>
-          <strong>
-            Please click the link below to finish the account creation process on the OCH Habitats
-            Digital Monitor Log website.
-          </strong>
-        </Item>
-        <Item>
-          <a href={`http://${url}`}>OC Habitats Digital Monitor Log site</a>
-          {/* <Button bgColor="ochBlue" w="410px" h="45px">
+    <Email title="">
+      <Image src={OCHLogo} w="346px" h="105px" alt="OC Habitats Logo" />
+      <Item>
+        <h1>{`Hello OC Habitats ${role === 'volunteer' ? 'Monitor' : 'Administrator'}!`}</h1>
+      </Item>
+      <Item>
+        <p>
+          An OC Habitats administrator has requested to grant you access to the OC Habitats Digital
+          Monitor Log system. To complete your registration, you will need to enter your first and
+          last name, and create a password.
+        </p>
+      </Item>
+      <Item>
+        <strong>
+          Please click the link below to finish the account creation process on the OCH Habitats
+          Digital Monitor Log website.
+        </strong>
+      </Item>
+      <Item>
+        <a href={`http://${url}`}>OC Habitats Digital Monitor Log site</a>
+        {/* <Button bgColor="ochBlue" w="410px" h="45px">
               Finish Account Creation
             </Button> */}
-        </Item>
-      </Email>
-    </div>
+      </Item>
+    </Email>
   );
 };
 
