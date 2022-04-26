d1832b08cdae58dc8e933338fd7aca047f1b86d9
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-04-26 01:40:15 -0500

    Admin Dashboard Page (#86)
    * Create PR for #77
    
    * Creating Admin Dashboard with Submission Stats and Emergent Issues
    
    * Tables and Notifications
    
    * Modal Pop Up For Monitor Log Submission Stats
    
    * Modal Popup For Emergent Issues Data
    
    * Changed Route for Admin Dashboard
    
    * New Design Changes with Popups and Unassigned Segments
    
    * Fixed Section Page duplication issue
    
    * Emergent Issue Cards to be 3 per Row instead of 4 per row
    
    Co-authored-by: parsakmt <parsakmt@users.noreply.github.com>
    Co-authored-by: Parsa Karamat <pkmtyp@gmail.com>
    Co-authored-by: Jane Vo <vo.janehanh@gmail.com>

---
+++ src/components/AdminDashboard/LogNotification.jsx
@@ -0,0 +1,34 @@
+import { React } from 'react';
+import { Text, Box, Button, Spacer, Flex } from '@chakra-ui/react';
+import { CheckCircleIcon, ArrowForwardIcon, CloseIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+
+const LogNotification = ({ numNotifications }) => {
+  return (
+    <Box bg="#C6F6D5" h="56px" maxH="56px" ml="110px" mr="110px">
+      <Flex h="100%" justify="space-between" align="center">
+        <CheckCircleIcon ml="18px" mr="14px" color="green" />
+        <Text fontSize="16px">
+          There are <b>{numNotifications}</b> monitor logs to review
+        </Text>
+        <Spacer />
+        <Link to="/logs">
+          <Button colorScheme="green" variant="solid" fontWeight="600">
+            <Text mr="8px">Review Logs</Text>
+            <ArrowForwardIcon />
+          </Button>
+        </Link>
+        <Button colorScheme="#C6F6D5" variant="ghost" ml="21.64px" mr="21.64px">
+          <CloseIcon fontSize="13px" />
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
+
+LogNotification.propTypes = {
+  numNotifications: PropTypes.number.isRequired,
+};
+
+export default LogNotification;
