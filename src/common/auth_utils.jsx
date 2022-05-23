bbbde3d6402798f592b3a04b7d08f5e12ff07760
Jane Vo <vo.janehanh@gmail.com>
2022-05-22 22:58:51 -0700

    Style forgot password

--- src/common/auth_utils.jsx
+++ src/common/auth_utils.jsx
@@ -205,10 +205,11 @@ const registerWithEmailAndPassword = async (
  */
 const sendPasswordReset = async email => {
   const user = await OCHBackend.get(`/users/email/${email}`);
-  if (user.response.status !== 200 || !user || !user.data) {
-    throw new Error(`There is no account associated with the email ${email}.`);
-  } else {
+  console.log(user);
+  if (user) {
     await sendPasswordResetEmail(auth, email);
+  } else {
+    throw new Error(`There is no account associated with the email ${email}.`);
   }
 };
 
--- src/components/Authentication/ForgotPassword.jsx
+++ src/components/Authentication/ForgotPassword.jsx
@@ -1,42 +1,74 @@
+import { Box, Container, Center, Text, Flex, Heading, Input, Button } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { sendPasswordReset } from '../../common/auth_utils';
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState();
   const [errorMessage, setErrorMessage] = useState();
-  const [confirmationMessage, setConfirmationMessage] = useState();
+  const [confirmed, setConfirmed] = useState(false);
+
   const handleForgotPassword = async e => {
     try {
       e.preventDefault();
       await sendPasswordReset(email);
-      setConfirmationMessage(
-        'If the email entered is associated with an account, you should receive an email to reset your password shortly.',
-      );
+      setConfirmed(true);
+      // setConfirmationMessage(
+      //   'If the email entered is associated with an account, you should receive an email to reset your password shortly.',
+      // );
       setErrorMessage('');
       setEmail('');
     } catch (err) {
       setErrorMessage(`Sorry! We couldn't find an account associated with the email ${email}.`);
     }
   };
+
   return (
-    <div>
-      <h2>Send Reset Email</h2>
-      <form onSubmit={handleForgotPassword}>
-        <input
-          type="text"
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
-          placeholder="Email"
-        />
-        <br />
-        <button type="submit">Send Email</button>
-      </form>
-      {confirmationMessage && <p>{confirmationMessage}</p>}
-      <a href="/">Back to Login</a>
-      <br />
-      <br />
-      {errorMessage && <p>{errorMessage}</p>}
-    </div>
+    <Container maxW="container.xl" mt="10vw" centerContent>
+      <Flex maxW="700px" align="center" direction="column" gap={5}>
+        <Heading>Forgot Password</Heading>
+        {!confirmed && (
+          <form onSubmit={handleForgotPassword}>
+            <Flex direction="column" bg="rgba(43, 192, 227, .10)" gap={3} p={12} borderRadius={6}>
+              <Text>
+                Enter your OC Habitats email, and we will send you instructions to reset your
+                password.
+              </Text>
+              <Box>
+                <Text>OC Habitats Email</Text>
+                <Input
+                  bg="white"
+                  value={email}
+                  onChange={({ target }) => setEmail(target.value)}
+                  placeholder="och@ochabitats.org"
+                />
+              </Box>
+              {errorMessage && (
+                <Text mt={0} color="red">
+                  {errorMessage}
+                </Text>
+              )}
+              <Button type="submit" bgColor="ochBlue" w="200px" alignSelf="flex-end">
+                Continue
+              </Button>
+            </Flex>
+          </form>
+        )}
+        {confirmed && (
+          <Flex direction="column" bg="rgba(43, 192, 227, .10)" gap={3} p={12} borderRadius={6}>
+            <Text>
+              An email has sent to allow you to reset your password. Please wait a few minutes for
+              the email to be delivered and follow the instructions. Check your spam and promotions
+              inboxes. If you do not receive an email within an hour, please re-enter your email and
+              try again.
+            </Text>
+            <Button bgColor="ochBlue" w="200px" alignSelf="flex-end">
+              <Link to="/login">Back to Login</Link>
+            </Button>
+          </Flex>
+        )}
+      </Flex>
+    </Container>
   );
 };
 
