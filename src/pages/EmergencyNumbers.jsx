841db388d15ab176821f4a7e0d1180fadd20bdfc
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-05-31 14:53:37 -0500

    Finish Emergency Numbers page (#143)
    * Create PR for #110
    
    * Volunteer view
    
    * Mobile table
    
    * Fix modal styling
    
    * Better mobile
    
    * More page styles
    
    * useDisclosure hook for new contact modal
    
    * Fix useEffect logic
    
    * Handle errors + toasts
    
    * Fix delete modal disclosure
    
    * Sticky last row
    
    * Fix volunteer mobile navbar dropdown
    
    * Mobile navbar fixes
    
    * isLoggedIn for mobile
    
    * Fixes
    
    * Change import
    
    Co-authored-by: janehvo <janehvo@users.noreply.github.com>
    Co-authored-by: Jane Vo <vo.janehanh@gmail.com>

--- src/pages/EmergencyNumbers.jsx
+++ src/pages/EmergencyNumbers.jsx
@@ -1,57 +1,84 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Stack, Text, VStack, Center } from '@chakra-ui/react';
+import { Box, Heading, Stack, Text, VStack, useToast } from '@chakra-ui/react';
 import NewNumberModal from '../components/NumberModals/NewNumberModal';
 import { OCHBackend } from '../common/utils';
 import EmergencyContactTable from '../components/NumberModals/EmergencyContactTable';
-
-// TODO:
-// - Move components into their own files/out of other components (EmergencyContactTable)
-// - Remove change, numbers state variable
-// - Convert EditAndDeleteModal to arrow function
-// - Replace Popover with Chakra Menu
-// - Clean up modal opening logic
-// - Maybe look into React Hook Form for validation
+import { useUserContext } from '../common/UserContext/UserContext';
 
 const Numbers = () => {
   const [change, setChange] = useState(true);
   const [tableData, setTableData] = useState([]);
 
+  const user = useUserContext();
+  const toast = useToast();
+
   const addNewNumber = async newNumber => {
-    await OCHBackend.post('/numbers', {
-      name: newNumber.name,
-      number: newNumber.number,
-      note: newNumber.note,
-    });
-    setChange(!change);
+    try {
+      await OCHBackend.post('/numbers', {
+        name: newNumber.name,
+        number: newNumber.number,
+        note: newNumber.note,
+      });
+      setChange(!change);
+      toast({
+        title: 'New contact added',
+        description: `${newNumber.name} (${newNumber.number}) is now a contact.`,
+        status: 'success',
+        isClosable: true,
+      });
+    } catch (err) {
+      toast({
+        title: 'Unable to add new contact',
+        description: err?.message,
+        status: 'error',
+        isClosable: true,
+      });
+    }
   };
 
   useEffect(async () => {
     const res = await OCHBackend.get('/numbers');
     setTableData(res.data);
-  }, []);
+  }, [change]);
 
   return (
-    <Center>
-      <Stack w="container.xl" justify-content="center" mb="4em">
-        <VStack align="left" spacing="1.5em" w="100%">
-          <Text fontWeight="600" fontSize="36px" mt="40px">
-            Emergency Numbers
-          </Text>
-          <Text fontWeight="500" fontSize="16px" mt="24px">
+    <Stack
+      mx={{ md: '10%', base: '10px' }}
+      mb={{ md: 10, base: 20 }}
+      spacing="20px"
+      maxW="1000px"
+      align="flex-start"
+    >
+      <Heading mt="20px">Emergency Numbers</Heading>
+      <Stack
+        w="100%"
+        direction={{ md: 'row', base: 'column' }}
+        justify="space-between"
+        align={{ md: 'flex-end', base: 'flex-start' }}
+      >
+        <Text p={0}>
+          <Text>
             In the case of an emergency or situation beyond your authorizations, call for
             assistance.
           </Text>
-          <Text>REMEMBER, YOUR SAFETY IS OUR PRIORITY.</Text>
-
+          <Text fontWeight="500">REMEMBER, YOUR SAFETY IS OUR PRIORITY.</Text>
+        </Text>
+        {user.userData.role === 'admin' && (
           <VStack spacing={2} align="stretch">
             <NewNumberModal addNewNumber={addNewNumber} />
           </VStack>
-          <Box w="900px">
-            <EmergencyContactTable tableData={tableData} setTableData={setTableData} />
-          </Box>
-        </VStack>
+        )}
       </Stack>
-    </Center>
+      <Box maxW="1000px" w={{ base: '100%' }}>
+        <EmergencyContactTable
+          tableData={tableData}
+          setTableData={setTableData}
+          admin={user.userData.role === 'admin'}
+          change={change}
+          setChange={setChange}
+        />
+      </Box>
+    </Stack>
   );
 };
 
