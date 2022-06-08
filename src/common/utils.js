eba7c5c7a3b15a96a631401e9b12e8aae22e8f0e
Zubair Sidhu <sidhuzubair@gmail.com>
2022-06-07 17:07:45 -0700

    Add prod url

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+let baseURL = '';
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  // dev code
+  baseURL = `${process.env.REACT_APP_API_URL}`;
+} else {
+  // production code
+  baseURL = `${process.env.REACT_APP_PROD_API_URL}`;
+}
+
 // See auth_utils for AuthInterceptor
 const OCHBackend = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}`,
+  baseURL,
 });
 
 // eslint-disable-next-line import/prefer-default-export
