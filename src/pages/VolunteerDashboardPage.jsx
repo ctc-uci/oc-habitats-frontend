f815ba312630d1f87da40f705f903cc9006a05a2
Jane Vo <70874803+janehvo@users.noreply.github.com>
2022-07-08 00:56:07 -0700

    Finish volunteer notifications (#160)
    * Notifcation component
    
    * Edits requested reminder
    
    * Monthly submission notif
    
    * Connect notification post
    
    * Fixes
    
    * another fix
    
    * fix typo...

--- src/pages/VolunteerDashboardPage.jsx
+++ src/pages/VolunteerDashboardPage.jsx
@@ -1,13 +1,6 @@
 /* eslint-disable react/prop-types */
-import { ArrowForwardIcon, InfoIcon } from '@chakra-ui/icons';
+import { InfoIcon } from '@chakra-ui/icons';
 import {
-  Alert,
-  AlertDescription,
-  AlertIcon,
-  AlertTitle,
-  Box,
-  Button,
-  CloseButton,
   Container,
   Flex,
   Heading,
@@ -19,68 +12,28 @@ import {
 } from '@chakra-ui/react';
 import { React, useEffect, useState } from 'react';
 import { OCHBackend } from '../common/utils';
+import { formatDate } from '../common/dateUtils';
 import RecentlySubmittedLog from '../components/VolunteerDashboard/RecentlySubmittedLog';
 import SegmentAssignment from '../components/VolunteerDashboard/SegmentAssignment';
 import UnsubmittedLogDraft from '../components/VolunteerDashboard/UnsubmittedLogDraft';
-
-// TODO: go to log button functionality
-
-// temporary (?) notification component until notification system is written
-// TODO: replace notification
-const Toast = props => {
-  const [closed, setClosed] = useState();
-  const { id, title, description, status, variant, closeButton, goToLogButton } = props;
-
-  const close = () => {
-    setClosed(true);
-    OCHBackend.delete(`/notification/${id}`, { withCredentials: true });
-  };
-
-  return closed ? (
-    ''
-  ) : (
-    <Alert borderRadius="md" status={status} variant={variant}>
-      <AlertIcon />
-      <Box flex="1">
-        <AlertTitle>{title}</AlertTitle>
-        <AlertDescription display="block">{description}</AlertDescription>
-        {closeButton && (
-          <CloseButton position="absolute" right="8px" top="8px" onClick={() => close()} />
-        )}
-        {goToLogButton && (
-          <Button float="right" colorScheme="red" size="sm" rightIcon={<ArrowForwardIcon />}>
-            Go to Log
-          </Button>
-        )}
-      </Box>
-    </Alert>
-  );
-};
+import Notification from '../components/VolunteerDashboard/Notification';
 
 const VolunteerDashboardPage = () => {
   const [userData, setUserData] = useState(null);
   const [userSubmissions, setUserSubmissions] = useState([]);
   const [userNotifications, setUserNotifications] = useState([]);
+  const currentDate = new Date();
 
   // Get data from backend
   useEffect(async () => {
     try {
-      const [userRes, submissionRes] = await Promise.all([
+      const [userRes, submissionRes, notificationsRes] = await Promise.all([
         OCHBackend.get('/users/me', { withCredentials: true }),
         OCHBackend.get('/users/userSubmissions', { withCredentials: true }),
+        OCHBackend.get('/notification/', { withCredentials: true }),
       ]);
       setUserData(userRes.data);
       setUserSubmissions(submissionRes.data);
-    } catch (err) {
-      // TODO: handle error
-      // eslint-disable-next-line no-console
-      console.log(err);
-    }
-  }, []);
-
-  useEffect(async () => {
-    try {
-      const notificationsRes = await OCHBackend.get('/notification/', { withCredentials: true });
       setUserNotifications(notificationsRes.data);
     } catch (err) {
       // TODO: handle error
@@ -90,17 +43,57 @@ const VolunteerDashboardPage = () => {
   }, []);
 
   const Notifications = () => {
-    return userNotifications.map(notification => (
-      <Toast
-        key={notification._id}
-        id={notification._id}
-        title={notification.title}
-        description={notification.message}
-        status={notification.type === 'MONITOR_LOG_APPROVED' ? 'success' : 'error'}
-        variant="left-accent"
-        closeButton={notification.type === 'MONITOR_LOG_APPROVED' ? 'true' : 'false'}
-      />
-    ));
+    // There are 3 kinds of notifications:
+    // segment submission status, approval notification, and edits requested reminder
+    const editsRequested = userSubmissions.filter(
+      submission => submission.status === 'EDITS_REQUESTED',
+    );
+
+    const submissionMonth = datetime => {
+      const submissionDate = new Date(datetime);
+      return submissionDate.getMonth();
+    };
+
+    const segments = userData.segments.map(segment => segment.segmentId);
+    const monthlySubmissions = userSubmissions
+      .filter(submission => submissionMonth(submission.submittedAt) === currentDate.getMonth())
+      .map(submission => submission.segment.segmentId);
+
+    const submitted = segments.filter(segment => monthlySubmissions.includes(segment));
+    const unsubmitted = segments.filter(segment => !monthlySubmissions.includes(segment));
+    const submissionDescription = unsubmitted.length
+      ? `You have not submitted a log for: ${unsubmitted.toString()}`
+      : 'You have submitted logs for all of your assigned sections!';
+    const submissionTitle = submitted.length
+      ? `You have submitted a log for: ${submitted.toString()}`
+      : 'You have not submitted any logs this month.';
+
+    return (
+      <>
+        <Notification title={submissionTitle} description={submissionDescription} type="status" />
+        {userNotifications.map(notification => (
+          <Notification
+            key={notification._id}
+            id={notification._id}
+            title={notification.message}
+            description="Thank you for your hard work, and keep it up!"
+            type="approved"
+            closeable
+          />
+        ))}
+        {editsRequested.map(requested => (
+          <Notification
+            key={requested._id}
+            title={`Edits have been requested for your ${
+              requested.segment.segmentId
+            } log on ${formatDate(requested.requestedEdits.requestDate)}`}
+            description={`Request Reason: ${requested.requestedEdits.requests}`}
+            type="changes"
+            logId={requested._id}
+          />
+        ))}
+      </>
+    );
   };
 
   const Segments = () => {
