cfeb703e14bd79b8d99981b48e9cf8b8379e00ac
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-07-21 14:40:07 -0500

    deployment config (#156)
    * Create PR for #155
    
    * Add prod url
    
    * Add package to disable react dev tools in prod
    
    Co-authored-by: ZubairSidhu <ZubairSidhu@users.noreply.github.com>
    Co-authored-by: Zubair Sidhu <sidhuzubair@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import './index.css';
 import App from './App';
+
+// Disable react developer tools in production
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.render(
   <React.StrictMode>
